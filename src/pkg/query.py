7e85d8f8c0ede13d0e76da1a99feef5ab89018ab
NikithaHebbar <nikitha.hebbar@maersk.com>
2025-03-21 23:18:31 +0530

    changes

--- src/pkg/query.py
+++ src/pkg/query.py
@@ -6,14 +6,8 @@ from typing import Optional
 
 llmstats = LLMStats(model_name=azure_openai_model)
 
-class Steps(BaseModel):
-    step1: str = Field(...,description="Identify the tables and join them based on their foreign keys")
-    step2: str = Field(...,description="Apply filters and conditions given in the question")
-    step3: Optional[str] = Field(..., description="Sort, group, or finalize the output if needed")
-    step4: Optional[str] = Field(..., description="Use LIMIT 1 for questions asking about a single most or top item.")
-
 class SQLGenerationReasoning(BaseModel):
-    steps: Steps = Field(...,description="Short chain of thought reasoning explaining the logic")
+    steps: list[str] = Field(...,description="Short chain of thought reasoning explaining the logic in 3-4 steps")
     final_answer: str = Field(...,description="the final SQL query to answer the user request")
 
 system_query = None
@@ -70,10 +64,6 @@ def get_query_azure_openai(question):
 
 def evaluate_model(model:SQLGenerationReasoning):
     logging.info("Steps invloved:")
-    logging.info(f"Step 1: {model.steps.step1}")
-    logging.info(f"Step 2: {model.steps.step2}")
-    if model.steps.step3:
-        logging.info(f"Step 3: {model.steps.step3}")
-    if model.steps.step4:
-        logging.info(f"Step 4: {model.steps.step4}")
+    for step in model.steps:
+        logging.info(step)
     return model.final_answer
