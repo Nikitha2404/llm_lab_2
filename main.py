834234b1a82c88bc9061a7d1a091bb31f1e1c6b0
NikithaHebbar <nikitha.hebbar@maersk.com>
2025-03-21 11:22:51 +0530

    improve chat history

--- main.py
+++ main.py
@@ -7,25 +7,20 @@ questions = [
     "Which seller has delivered the most orders to customers in Rio de Janeiro? [string: seller_id]",
     "What's the average review score for products in the 'beleza_saude' category? [float: score]",
     "How many sellers have completed orders worth more than 100,000 BRL in total? [integer: count]",
-    "Which product category has the highest rate of 5-star reviews? [string: category_name]",
+    "Which product with atleast 100 reviews category has the highest rate of 5-star reviews? [string: category_name]",
     "What's the most common payment installment count for orders over 1000 BRL? [integer: installments]",
-    "Which city has the highest average freight value per order? [string: city_name]",
+    "Which city with atleast 50 orders has the highest average freight value per order? [string: city_name]",
     "What's the most expensive product category based on average price? [string: category_name]",
     "Which product category has the shortest average delivery time? [string: category_name]",
     "How many orders have items from multiple sellers? [integer: count]",
     "What percentage of orders are delivered before the estimated delivery date? [float: percentage]",
 ]
 
-messages =[
-    {"role":"system","content":get_system_query()}
-]
-
 if __name__ == "__main__":
     setup_logging(output_dir="/Users/nikitha.hebbar/Documents/llm_lab_week2")
     for question in questions:
         print("-----------------------------------------------------------------\n")
-        sql_query = get_query_azure_openai(messages,question)
-        logging.info(messages)
+        sql_query = get_query_azure_openai(question)
         if sql_query:
             print(sql_query)
             query_result= execute_query(sql_query)
--- src/pkg/query.py
+++ src/pkg/query.py
@@ -6,26 +6,45 @@ class SQLGenerationReasoning(BaseModel):
     steps: list[str] = Field(...,description="Short chain of thought reasoning explaining the logic in 3 steps")
     final_answer: str = Field(...,description="the final SQL query to answer the user request")
 
+system_query = None
 def get_system_query():
-    with open('/Users/nikitha.hebbar/Documents/llm_lab_week2/prompts/system.txt', 'r', encoding='utf-8') as file:
-        systemQuery = file.read() 
-    return systemQuery
+    # singleton
+    global system_query
+    if system_query is None:  
+        with open('/Users/nikitha.hebbar/Documents/llm_lab_week2/prompts/system.txt', 'r', encoding='utf-8') as file:
+            system_query = file.read()
+    return system_query
 
-def build_chain_of_thought_messages(messages: list[dict] | None, answer: str | None):
+messages =[
+    {"role":"system","content":get_system_query()}
+]
+
+def trim_messages(messages: list[dict]):
+    system_message = messages[0] # system promt
+    chat_history = [m for m in messages if m["role"] in {"user", "assistant"}][-6:]  # Keep last 3 user-assistant pairs
+
+    return [system_message] + chat_history
+
+def build_chain_of_thought_messages(answer: str | None):
+    global messages
     if answer != None:
         answer_dict = {"role":"assistant","content":answer}
     messages.append(answer_dict)
+    messages = trim_messages(messages=messages)
     return messages
 
-def build_messages(messages: list[dict] | None,question: str|None):
+def build_messages(question: str|None):
+    global messages
     if question!=None:
         question_dict={"role":"user","content":question}
     messages.append(question_dict)
     return messages
 
-def get_query_azure_openai(messages,question):
-    messages=build_messages(messages,question)
+def get_query_azure_openai(question):
+    global messages
+    messages=build_messages(question)
     try:
+        logging.info(messages)
         logging.info(question)
         completion = client.beta.chat.completions.parse(
             model=azure_openai_model,
@@ -33,7 +52,7 @@ def get_query_azure_openai(messages,question):
             response_format=SQLGenerationReasoning
         )
         answer=evaluate_model(completion.choices[0].message.parsed)
-        messages=build_chain_of_thought_messages(messages,answer)
+        messages=build_chain_of_thought_messages(answer)
         return answer
     except Exception as e:
         raise ConnectionError(e)
